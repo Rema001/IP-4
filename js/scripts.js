9cb1bf2ad7ae01ea6776fffed2fd5f110270354f
Rema001 <indohmike@gmail.com>
2020-08-03 03:01:22 +0300

    added document ready function

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+// business logic
+
+function pizzaOrder (name,size,crust,topping)
+{
+  this.name =name;
+  this.size =size;
+  this.crust = crust;
+  this.topping = topping;
+}
+
+pizzaOrder.prototype.total= function(){
+  return this.size+this.crust+this.topping;
+
+}
+
+
+// user logic
+
+$(document).ready(function(){
+
+
+
